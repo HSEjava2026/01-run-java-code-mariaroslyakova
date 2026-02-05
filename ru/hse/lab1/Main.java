1a0ac9227e7c175051752ff1188eefacb4a5839b
Maria <maria.roslyakova@bk.ru>
2026-02-05 17:38:22 +0300

    1-2

---
+++ ru/hse/lab1/Main.java
@@ -0,0 +1,11 @@
+import second.Person;
+
+public class Main {
+    public static void main(String[] args) {
+
+    Person Ivan = new Person("Иван", 20);
+    System.out.println("Имя: " + Ivan.name);
+    System.out.println("Возраст: " + Ivan.age);
+    
+    }
+}
